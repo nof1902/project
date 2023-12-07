75672f8b74d2af31767dfe88311fa3d496601313
nof1902 <nof1902@gmail.com>
2023-12-07 17:42:54 +0200

    cofigs

--- web-dev/starter-proj/vite.config.js
+++ web-dev/starter-proj/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project/web-dev/starter-proj",
+  base: "/starter-proj",
   plugins: [react()],
 })
